ae4bbbf3529fff087c10e15d1a77cc140c214b8e
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-11-14 21:33:07 +0530

    Update image storage from drive to cloudinary

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,136 +1,102 @@
-import Post from "../models/postModel.js"
-import jwt from "jsonwebtoken";
-import fs, { unlink } from "fs";
-import path from "path";
-import { v4 as uuidv4 } from "uuid";
-import { fileURLToPath } from 'url';
+import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 import HttpError from "../models/errorModel.js";
-import { dirname, join } from 'path';
-import {google} from 'googleapis';
+import cloudinary from "../config/cloudinary.js";
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URI
-);
-oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-const drive = google.drive({
-  version: 'v3',
-  auth: oauth2Client
-});
-
-// create new post
-
+// ---------------------- CREATE POST ----------------------
 
 const createPost = async (req, res, next) => {
   try {
-    let { title, category, description } = req.body;
-    if (!title || !category || !description || !req.files || req.files.length === 0) {
-      throw new HttpError("Fill in all the fields and upload a file", 422);
+    const { title, category, description } = req.body;
+
+    if (!title || !category || !description || !req.files?.thumbnail) {
+      throw new HttpError("Fill all fields and upload an image", 422);
     }
+
     const thumbnail = req.files.thumbnail;
-    if (!thumbnail || thumbnail.size === undefined) {
-      throw new HttpError("Image not provided. Please upload an image.", 422);
+
+    if (thumbnail.size > 2_000_000) {
+      throw new HttpError("Image too big. Should be less than 2MB", 422);
     }
-    if (thumbnail.size > 2000000) {
-      throw new HttpError("Image too big. It should be less than 2 MB.", 422);
-    }
-    let fileName = thumbnail.name;
-    let splittedFileName = fileName.split('.');
-    let newFileName = splittedFileName[0] + uuidv4() + '.' + splittedFileName[splittedFileName.length - 1];
-    const filePath = path.join(__dirname, '..', '/uploads', newFileName);
-    thumbnail.mv(filePath, async (err) => {
-      if (err) {
-        throw new HttpError(err);
-      } else {
-        if (oauth2Client.isTokenExpiring()) {
-          const { credentials } = await oauth2Client.refreshAccessToken();
-          oauth2Client.setCredentials(credentials);
-        }
-        const response = await drive.files.create({
-          requestBody: {
-            name: newFileName,
-            mimeType: 'image/jpeg'
-          },
-          media: {
-            mimeType: 'image/jpeg',
-            body: fs.createReadStream(filePath)
-          },
-          fields: 'id' 
-        });
-        const fileId = response.data.id;
-        await drive.permissions.create({
-          fileId: fileId,
-          requestBody: {
-            role: 'reader',
-            type: 'anyone'
-          }
-        });
-        const newPost = await Post.create({
-          title,
-          category,
-          description,
-          thumbnail: fileId,
-          authorId: req.user.id
-        });
-        const currentUser = await User.findById(req.user.id);
-        const userPostCount = currentUser.posts + 1;
-        await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
-        res.status(200).json(newPost);
-      }
+
+    // Unique temp name
+    const tempName = Date.now() + "_" + thumbnail.name;
+    const tempPath = join(__dirname, "..", "uploads", tempName);
+    await thumbnail.mv(tempPath);
+
+    // Upload to Cloudinary
+    const cloudUpload = await cloudinary.uploader.upload(tempPath, {
+      folder: "blog",
     });
+
+    // Delete temp file
+    fs.unlinkSync(tempPath);
+
+    const newPost = await Post.create({
+      title,
+      category,
+      description,
+      thumbnail: cloudUpload.secure_url,
+      authorId: req.user.id,
+    });
+
+    await User.findByIdAndUpdate(req.user.id, { $inc: { posts: 1 } });
+
+    res.status(200).json(newPost);
   } catch (error) {
     next(error);
   }
 };
 
-
-
-
-
-
-
-// edit existing post
-
+// ----------------------- EDIT POST -----------------------
 
 const editPost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { title, description, category } = req.body;
-    
+
     if (!title || !category || !description || description.length < 12) {
-      throw new HttpError("Fill in all the fields and ensure description is at least 12 characters long", 422);
+      throw new HttpError("Fill all fields correctly", 422);
     }
 
-    const userId = req.user.id;
-    console.log(id);
-    console.log(userId);
-    const post = await Post.findOne({ _id: id, authorId: userId });
+    const post = await Post.findOne({ _id: id, authorId: req.user.id });
     if (!post) {
-      throw new HttpError("Post not found or you do not have permission to edit it", 404);
+      throw new HttpError("Post not found or unauthorized", 404);
     }
 
-    let updatedPost;
-    let thumbnailId;
-    
-    if (req.files && req.files.thumbnail) {
+    let thumbnailUrl = post.thumbnail;
+
+    if (req.files?.thumbnail) {
       const thumbnail = req.files.thumbnail;
-      
-      if (thumbnail.size > 2000000) {
-        throw new HttpError("Image too big. It should be less than 2 MB.", 422);
+
+      if (thumbnail.size > 2_000_000) {
+        throw new HttpError("Image too big. Should be less than 2MB", 422);
       }
-      
-      thumbnailId = await uploadThumbnailToDrive(thumbnail);
+
+      const tempName = Date.now() + "_" + thumbnail.name;
+      const tempPath = join(__dirname, "..", "uploads", tempName);
+      await thumbnail.mv(tempPath);
+
+      const cloudUpload = await cloudinary.uploader.upload(tempPath, {
+        folder: "blog",
+      });
+
+      fs.unlinkSync(tempPath);
+
+      thumbnailUrl = cloudUpload.secure_url;
     }
 
-    updatedPost = await Post.findByIdAndUpdate(id, { title, description, category, thumbnail: thumbnailId }, { new: true });
-
-    if (!updatedPost) {
-      throw new HttpError("Could not update post", 500);
-    }
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { title, description, category, thumbnail: thumbnailUrl },
+      { new: true }
+    );
 
     res.status(200).json(updatedPost);
   } catch (error) {
@@ -138,159 +104,86 @@ const editPost = async (req, res, next) => {
   }
 };
 
-async function uploadThumbnailToDrive(thumbnail) {
-  const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
-  oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
-
-  const drive = google.drive({
-    version: 'v3',
-    auth: oauth2Client
-  });
-
-  const fileName = thumbnail.name;
-  const filePath = path.join(__dirname, '..', '/uploads', fileName);
-
-  await thumbnail.mv(filePath);
-
-  try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: fileName,
-        mimeType: 'image/jpeg'
-      },
-      media: {
-        mimeType: 'image/jpeg',
-        body: fs.createReadStream(filePath)
-      },
-      fields: 'id'
-    });
-
-    return response.data.id;
-  } catch (error) {
-    console.log(error);
-    throw new HttpError("Error uploading image to Google Drive", 500);
-  }
-}
-
-
-// delete a post
+// ----------------------- DELETE POST -----------------------
 
 const deletePost = async (req, res, next) => {
   try {
     const { id } = req.params;
-    if (!id) {
-      throw new HttpError("Post ID is unavailable", 400);
-    }
 
     const post = await Post.findById(id);
     if (!post) {
       throw new HttpError("Post not found", 404);
     }
-    deleteThumbnailFromDrive(post);
-    await Post.findByIdAndDelete(id);
-      const currentUser = await User.findById(req.user.id);
-      if (currentUser) {
-        const userPostCount = currentUser.posts - 1;
-        await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
-      }
 
-      res.json("Post deleted successfully");
+    // Extract Cloudinary public_id
+    const urlParts = post.thumbnail.split("/");
+    const fileName = urlParts.pop().split(".")[0];
+    const publicId = `blog/${fileName}`;
+
+    await cloudinary.uploader.destroy(publicId);
+
+    await Post.findByIdAndDelete(id);
+
+    await User.findByIdAndUpdate(req.user.id, { $inc: { posts: -1 } });
+
+    res.json("Post deleted successfully");
   } catch (error) {
     next(error);
   }
 };
 
-const deleteThumbnailFromDrive = async (post) => {
-  try {
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URI
-    );
-    oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
-    const drive = google.drive({ version: 'v3', auth: oauth2Client });
-    const fileId = await post.thumbnail;
-    const response = await drive.files.delete({ fileId });
-
-    return response;
-  } catch (error) {
-    console.error("Error deleting file from Google Drive:", error);
-    throw error;
-  }
-};
-
-const findFileIdByName = async (drive, fileName) => {
-  try {
-    const response = await drive.files.list({
-      q: `name='${fileName}'`,
-      fields: 'files(id)',
-    });
-
-    if (response.data.files.length > 0) {
-      return response.data.files[0].id;
-    } else {
-      throw new Error(`File '${fileName}' not found in Google Drive`);
-    }
-  } catch (error) {
-    console.error("Error finding file ID:", error);
-    throw error;
-  }
-};
-
-
-// get all posts
-
+// ----------------------- GETS -----------------------
 
 const getPosts = async (req, res, next) => {
   try {
-    const posts = await Post.find({}).sort({ createdAt: -1 }); // Sort by createdAt in descending order
-    res.status(200).json(posts);
+    const posts = await Post.find({}).sort({ createdAt: -1 });
+    res.json(posts);
   } catch (error) {
     next(error);
   }
 };
 
-// get posts by author
-
-const authorPosts= async (req,res,next)=>{
+const authorPosts = async (req, res, next) => {
   try {
-    const {id} = req.params
-    const authorPosts = await Post.find({authorId:id}).sort({createdAt:-1})
-    res.status(200).json(authorPosts)
+    const posts = await Post.find({ authorId: req.params.id }).sort({
+      createdAt: -1,
+    });
+    res.json(posts);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-// get posts by category
-
-const categoryPosts= async (req,res,next)=>{
+const categoryPosts = async (req, res, next) => {
   try {
-    const {category} = req.params
-    const categoryPosts = await Post.find({category:category}).sort({createdAt:-1});
-    if(categoryPosts.length==0){
-      throw new HttpError('Posts not found in this category',404)
-    }
-    res.status(200).json(categoryPosts)
+    const posts = await Post.find({ category: req.params.category }).sort({
+      createdAt: -1,
+    });
+
+    if (!posts.length) throw new HttpError("No posts in this category", 404);
+
+    res.json(posts);
   } catch (error) {
-    throw new HttpError(error)
+    next(error);
   }
-}
+};
 
-// get a single post
-
-const getPost= async (req,res,next)=>{
+const getPost = async (req, res, next) => {
   try {
-    const {id} = req.params
-    const post = await Post.findById(id)
-    if(!post){
-      throw new HttpError("Post not found",404)
-    }
-    res.status(200).json(post)
+    const post = await Post.findById(req.params.id);
+    if (!post) throw new HttpError("Post not found", 404);
+    res.json(post);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-
-export default {getPost,getPosts,editPost,deletePost,authorPosts,categoryPosts,createPost}
+export default {
+  getPost,
+  getPosts,
+  editPost,
+  deletePost,
+  authorPosts,
+  categoryPosts,
+  createPost,
+};
